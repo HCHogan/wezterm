71ee3dff3c27c27248074f7fa12a77f08476b73d
Hank Hogan <ysh2291939848@outlook.com>
2023-10-06 22:10:09 +0800

    ok

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,41 @@
 local wezterm = require("wezterm")
 
+-- local format_title = function(title, is_active, max_width)
+-- 	local background = { Background = { Color = "#1f1f28" } }
+-- 	local title_len = #title
+-- 	local pad_len = math.floor((max_width - title_len) / 2)
+--
+-- 	local formatted_title = {
+-- 		Text = string.rep(" ", pad_len) .. title .. string.rep(" ", pad_len),
+-- 	}
+-- 	if is_active then
+-- 		return { background, { Foreground = { Color = "#957fb8" } }, formatted_title }
+-- 	else
+-- 		return { background, { Foreground = { Color = "#cad3f5" } }, formatted_title }
+-- 	end
+-- end
+--
+-- local user_var_tab_title_key = "tab_title"
+-- wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+-- 	-- if there is title already set, proceed with it
+-- 	if type(tab.tab_title) == "string" and #tab.tab_title > 0 then
+-- 		return format_title(tab.tab_title, tab.is_active, max_width)
+-- 	end
+-- 	return format_title("temp", tab.is_active, max_width)
+-- end)
+--
+-- wezterm.on("update-right-status", function(window)
+-- 	local date = wezterm.strftime("%Y-%m-%d %H:%M:%S")
+-- 	window:set_right_status({ Foreground = { Color = "#cad3f5" } }, wezterm.format({ { Text = " " .. date .. " " } }))
+-- end)
+--
+-- wezterm.on("user-var-changed", function(window, pane, name, value)
+-- 	wezterm.log_info("user-var-changed", name, value)
+-- 	if name == user_var_tab_title_key then
+-- 		pane:tab():set_title(value)
+-- 	end
+-- end)
+
 local function font_with_fallback(name, params)
 	local names = { name, "Apple Color Emoji", "azuki_font" }
 	return wezterm.font_with_fallback(names, params)
@@ -15,14 +51,18 @@ custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
 custom.tab_bar.new_tab.bg_color = "#080808"
 
 return {
-	front_end = "WebGpu",
+	front_end = "OpenGL",
+	-- window_decorations = "INTEGRATED_BUTTONS|RESIZE",
 	window_decorations = "RESIZE",
+	native_macos_fullscreen_mode = true,
 
 	color_schemes = {
 		["OLEDppuccin"] = custom,
 	},
 
 	color_scheme = "Catppuccin Mocha",
+	initial_cols = 96,
+	initial_rows = 32,
 
 	-- Font config
 	font = font_with_fallback(font_name),
@@ -148,6 +188,11 @@ return {
 			mods = "CTRL|SHIFT",
 			action = wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }),
 		},
+		{
+			key = "n",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ToggleFullScreen,
+		},
 	},
 
 	-- Aesthetic Night Colorscheme
@@ -164,7 +209,8 @@ return {
 	enable_tab_bar = true,
 	hide_tab_bar_if_only_one_tab = true,
 	show_tab_index_in_tab_bar = false,
-	tab_bar_at_bottom = true,
+	use_fancy_tab_bar = true,
+	-- tab_bar_at_bottom = true,
 
 	-- General
 	automatically_reload_config = true,
