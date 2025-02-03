a3f26b232277041a04796445e721325615785e17
Hank Hogan <ysh2291939848@outlook.com>
2025-02-03 05:14:07 +0000

    yes

--- wezterm.lua
+++ wezterm.lua
@@ -175,9 +175,9 @@ local config = {
   bold_brightens_ansi_colors = true,
   -- Padding
   window_padding = {
-    left = 0,
+    left = 5,
     right = 0,
-    top = 0,
+    top = 5,
     bottom = 0,
   },
 
