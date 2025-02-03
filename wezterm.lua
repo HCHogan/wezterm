44dd2cd2f1e999413988f23f00e92b0c60633f64
Hank Hogan <ysh2291939848@outlook.com>
2025-02-03 05:09:44 +0000

    padding

--- wezterm.lua
+++ wezterm.lua
@@ -175,10 +175,10 @@ local config = {
   bold_brightens_ansi_colors = true,
   -- Padding
   window_padding = {
-    left = 5,
-    right = 5,
-    top = 5,
-    bottom = 5,
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
   },
 
   -- Tab Bar
